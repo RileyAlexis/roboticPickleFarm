43d4a959fd127ea1a375b858057f3bb090c69e4c
RileyAlexis <MxRileyAlexis@gmail.com>
2023-11-08 20:45:05 -0600

    database deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,10 +1,21 @@
 const dotenv = require('dotenv').config();
 const pg = require('pg');
 
-const pool = new pg.Pool({
-    host: process.env.HOST_NAME,
+let pool;
+
+if (process.env.DATABSE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABSE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }) 
+} else {
+    pool = new pg.Pool({
+    host: 'localhost',
     port: 5432,
-    database: process.env.RENDER_DB, 
+    database: 'robotic_pickle_farm', 
 });
+}
 
 module.exports = pool;
