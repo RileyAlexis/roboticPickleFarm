fb14353cafedd687dad1b1b74aff4ceccb9bf25b
RileyAlexis <MxRileyAlexis@gmail.com>
2024-01-23 11:59:44 -0600

    modified pool to accept nginx enviornment variables

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,19 +2,21 @@ const pg = require('pg');
 
 let pool;
 console.log(process.env.DATABASE_URL);
-if (process.env.DATABASE_URL) {
+if (process.env.NODE_ENV === 'production') {
     pool = new pg.Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }) 
+        user: process.env.PGUSER,
+        host: 'localhost',
+        port: 5432,
+        database: 'robotic_pickle_farm',
+    })
+
+
 } else {
     pool = new pg.Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'robotic_pickle_farm', 
-});
+        host: 'localhost',
+        port: 5432,
+        database: 'robotic_pickle_farm',
+    });
 }
 
 module.exports = pool;
