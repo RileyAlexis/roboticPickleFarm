3ad995d54701e933d586abd17b9b4d9d13f27c4d
RileyAlexis <MxRileyAlexis@gmail.com>
2024-01-26 13:32:01 -0600

    pool tweaks

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,11 @@
 const pg = require('pg');
 
 let pool;
-console.log('Nginx Node env', process.env.NODE_ENV);
-console.log('PG', process.env.PGUSER);
 if (process.env.NODE_ENV === 'production') {
+    console.log('Nginx Node env', process.env.NODE_ENV);
+    console.log('PG', process.env.PGUSER);
+    console.log('PG', typeof process.env.PASSWORD, process.env.PASSWORD);
+
     pool = new pg.Pool({
         user: process.env.PGUSER,
         password: process.env.PASSWORD,
